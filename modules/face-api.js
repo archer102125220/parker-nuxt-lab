16c3f269d7e48757569210408fbaf431ee765570
Parker <archer102125220.2015@gmail.com>
2024-11-14 17:52:40 +0800

    [update]: face-api import

--- modules/face-api.js
+++ modules/face-api.js
@@ -2,6 +2,13 @@
 import { dirname, join } from 'path';
 import { fileURLToPath } from 'url';
 import { defineNuxtModule, useNitro } from '@nuxt/kit';
+
+// import nodejs bindings to native tensorflow,
+// not required, but will speed up things drastically (python required)
+import '@tensorflow/tfjs-node';
+
+// implements nodejs wrappers for HTMLCanvasElement, HTMLImageElement, ImageData
+import * as canvas from 'canvas';
 import * as faceapi from 'face-api.js';
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
@@ -11,8 +18,16 @@ export default defineNuxtModule({
 
     nuxt.hook('ready', async () => {
       const nitroApp = useNitro();
+
+      // patch nodejs environment, we need to provide an implementation of
+      // HTMLCanvasElement and HTMLImageElement
+      const { Canvas, Image, ImageData } = canvas
+      faceapi.env.monkeyPatch({ Canvas, Image, ImageData })
       await faceapi.nets.ssdMobilenetv1.loadFromDisk(join(__dirname, '../public/models'));
+
       nitroApp.$faceapi = faceapi;
+
+      nuxt.$faceapi = faceapi;
 
     });
   }
