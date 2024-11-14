10b57c0cdca9d1fae8e672de1d6c27060937381f
Parker <archer102125220.2015@gmail.com>
2024-11-14 17:52:30 +0800

    [update]:  web-cam update

--- pages/web-cam.vue
+++ pages/web-cam.vue
@@ -21,13 +21,6 @@
 </template>
 <script setup>
 // https://mrcodingroom.freesite.host/js%E5%B0%87video-webcam%E7%95%AB%E5%9C%A8canvas%E4%B8%8A/
-const CONSTRAINTS = {
-  audio: false,
-  video: {
-    facingMode: 'user'
-  }
-};
-
 const videoEl = useTemplateRef('videoEl');
 const canvasEl = useTemplateRef('canvasEl');
 
@@ -36,6 +29,8 @@ const streamObj = ref(null);
 function handleFrameFromVideo() {
   const video = videoEl.value;
   const canvas = canvasEl.value;
+
+  if (typeof canvas?.getContext !== 'function') return;
 
   const ctx = canvas.getContext('2d');
   ctx.clearRect(0, 0, canvas.width, canvas.height);
@@ -51,15 +46,49 @@ function handleFrameFromVideo() {
 }
 
 async function getCameraStream() {
-  // 要 camera 的權限
-  const stream = await window.navigator.mediaDevices.getUserMedia(CONSTRAINTS);
-  // videoEl.value.srcObject = stream;
-  streamObj.value = stream;
+  try {
+    // 要 camera 的權限
+    const stream = await window.navigator.mediaDevices.getUserMedia({
+      audio: false,
+      video: {
+        facingMode: 'user'
+      }
+    });
+    console.log({ stream });
+    // videoEl.value.srcObject = stream;
+    streamObj.value = stream;
+    handleFrameFromVideo();
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+function handleStopStream(stream) {
+  if (typeof stream?.getTracks !== 'function') return;
+
+  const tracks = stream.getTracks();
+  tracks.forEach((track) => {
+    track.stop();
+    track.clone();
+    if (typeof stream?.removeTrack === 'function') {
+      stream.removeTrack(track);
+    }
+  });
 }
 
 onMounted(() => {
   getCameraStream();
-  handleFrameFromVideo();
+});
+
+onBeforeUnmount(() => {
+  if (typeof streamObj.value?.getTracks === 'function') {
+    handleStopStream(streamObj.value);
+
+    streamObj.value = null;
+    if (typeof videoEl.value === 'object') {
+      videoEl.value.srcObject = null;
+    }
+  }
 });
 </script>
 <style lang="scss" scoped>
