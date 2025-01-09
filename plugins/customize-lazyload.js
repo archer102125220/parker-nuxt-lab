05ed22d6fdf1997a87993d4844102e7ee3c15220
Parker <archer102125220.2015@gmail.com>
2025-01-09 16:33:52 +0800

    [update]: customize-lazyload update

--- plugins/customize-lazyload.js
+++ plugins/customize-lazyload.js
@@ -3,14 +3,24 @@ export default defineNuxtPlugin((nuxtApp) => {
   let observer = null;
   if (typeof window?.IntersectionObserver === 'function') {
     function handleLazyload(element) {
-      const src = element.getAttribute('lazy-src') || element.src || element.removeAttribute('error-img') || '';
+      const src = element.getAttribute('lazy-src') || element.src || element.removeAttribute('lazy-error-img') || '';
       if (src !== '') {
         element.src = src;
         element.removeAttribute('lazy-src');
       }
 
+      // if (element.getAttribute('has-loading-src') === 'false') {
+      //   element.style.opacity = null;
+      //   element.removeAttribute('has-loading-src');
+      // }
+
       element.addEventListener('load', function (event) {
-        event.target.removeAttribute('error-img');
+        event.target.removeAttribute('lazy-error-img');
+        if (element.getAttribute('has-loading-src') === 'false') {
+          element.style.opacity = null;
+          element.removeAttribute('has-loading-src');
+        }
+        element.setAttribute('lazy-finish', 'true');
       });
     }
     observer = new window.IntersectionObserver((entries) => {
@@ -26,29 +36,38 @@ export default defineNuxtPlugin((nuxtApp) => {
     });
   }
 
-
   function handleImgError(event) {
-    const errorImgSrc = event.target.getAttribute('error-img') || '';
+    const errorImgSrc = event.target.getAttribute('lazy-error-img') || '';
+    event.target.setAttribute('lazy-error', 'true');
     if (typeof errorImgSrc === 'string' && errorImgSrc !== '') {
       event.target.src = errorImgSrc;
-      event.target.removeAttribute('error-img');
+      event.target.removeAttribute('lazy-error-img');
       event.target.removeAttribute('lazy-src');
     }
+    event.target.removeEventListener('error', handleImgError);
+
     observer.unobserve(event.target);
   }
 
   const customizeLazyload = {
     mounted(el, binding) {
       const src = el.src || '';
-      const lazySrc = binding.value?.src || binding.value || src;
+      const lazySrc = (typeof binding.value?.src === 'string' ? binding.value?.src : binding.value) || src;
       if (typeof observer?.observe !== 'function') {
         el.src = lazySrc;
         return;
       }
 
+      if ((typeof binding.value?.loading !== 'string' && typeof binding.value?.loading !== 'object') || binding.value?.loading === '' || binding.value?.loading === null) {
+        el.style.opacity = 0;
+        el.setAttribute('has-loading-src', false);
+      } else {
+        el.setAttribute('has-loading-src', true);
+      }
+
       el.setAttribute('lazy-src', lazySrc);
       const errorImg = binding.value?.errorImg || config?.error || src;
-      el.setAttribute('error-img', errorImg);
+      el.setAttribute('lazy-error-img', errorImg);
 
       el.src = binding.value?.loading || config?.loading || src;
 
@@ -61,20 +80,40 @@ export default defineNuxtPlugin((nuxtApp) => {
       }
     },
     updated(el, binding) {
-      if (binding.value === binding.oldValue || binding.value?.src === binding.oldValue?.src) {
+      if ((binding.value === binding.oldValue || binding.value?.src === binding.oldValue?.src) && binding.value?.error === binding.oldValue?.error) {
         return;
       }
 
+      if (el.getAttribute('lazy-finish') === 'true') {
+        const newLazySrc = (typeof binding.value?.src === 'string' ? binding.value?.src : binding.value) || '';
+        const newErrorSrc = binding.value?.error || newLazySrc;
+
+        if (el.getAttribute('lazy-error') === 'true') {
+          el.src = newErrorSrc;
+        } else {
+          el.src = newLazySrc;
+        }
+
+        return
+      }
+
       const src = el.src || '';
-      const lazySrc = binding.value?.src || binding.value || src;
+      const lazySrc = (typeof binding.value?.src === 'string' ? binding.value?.src : binding.value) || src;
       if (typeof observer?.observe !== 'function') {
         el.src = lazySrc;
         return;
       }
 
+      if ((typeof binding.value?.loading !== 'string' && typeof binding.value?.loading !== 'object') || binding.value?.loading === '' || binding.value?.loading === null) {
+        el.style.opacity = 0;
+        el.setAttribute('has-loading-src', false);
+      } else {
+        el.setAttribute('has-loading-src', true);
+      }
+
       el.setAttribute('lazy-src', lazySrc);
       const errorImg = binding.value?.errorImg || config?.error || src;
-      el.setAttribute('error-img', errorImg);
+      el.setAttribute('lazy-error-img', errorImg);
 
       el.src = binding.value?.loading || config?.loading || src;
 
