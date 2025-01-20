da2df4b95b4236d584d1e16262b4573c41a2f53a
Parker <archer102125220.2015@gmail.com>
2025-01-20 17:09:03 +0800

    [update]: default layout update

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="defalut_layout">
     <LayoutHeader />
-    <main :class="mainClassName">
+    <main class="defalut_layout-content">
       <slot />
     </main>
     <!-- <LayoutFooter /> -->
@@ -9,46 +9,24 @@
 </template>
 
 <script setup>
-const { $store } = useNuxtApp();
-const route = useRoute();
-const getRouteBaseName = useRouteBaseName();
+// const { $store } = useNuxtApp();
+// const route = useRoute();
+// const getRouteBaseName = useRouteBaseName();
 
-const mainClassName = computed(() =>
-  getRouteBaseName(route) === 'index' || $store.system.isIndexPage === true
-    ? 'defalut_layout-home_content'
-    : 'defalut_layout-content'
-);
+// const mainClassName = computed(() =>
+//   getRouteBaseName(route) === 'index' || $store.system.isIndexPage === true
+//     ? 'defalut_layout-home_content'
+//     : 'defalut_layout-content'
+// );
 </script>
 
 <style lang="scss" scoped>
 .defalut_layout {
-  // height: 100vh;
-  width: 100vw;
-  // width: 1210px;
-  // margin: auto;
-  overflow: auto;
-  @include mobile {
-    // height: auto;
-    overflow: unset;
-    // width: 327px;
-  }
+  // width: 100vw;
   &-content {
-    width: 1210px;
-    max-width: 100%;
-    min-height: 80vh;
-    // margin: 0 40px;
-    margin: auto;
-    @include mobile {
-      width: 327px;
-      // margin: 0 20px;
-    }
-    @include tabletOnly {
-      width: calc(100% - 20px);
-      margin: 20px 10px;
-    }
-  }
-  &-home_content {
     min-height: 100vh;
+    width: 80%;
+    margin: auto;
   }
 }
 </style>
