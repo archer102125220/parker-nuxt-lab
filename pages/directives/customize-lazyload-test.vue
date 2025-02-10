f248599f6e2fcd0395dd65d1e3932c31cf0eb1b7
Parker <archer102125220.2015@gmail.com>
2025-02-10 09:41:58 +0800

    [update]: add test img

--- pages/directives/customize-lazyload-test.vue
+++ pages/directives/customize-lazyload-test.vue
@@ -2,8 +2,8 @@
   <div>
     <!-- <div style="height: 50vh" /> -->
     <img
-      src="/img/1e0ef282c7831f762deb4b4ded8592d5ff7962d832cebcf11709ae670e721560.jpg"
+      src="/img/test-img/1e0ef282c7831f762deb4b4ded8592d5ff7962d832cebcf11709ae670e721560.jpg"
     />
-    <img v-customize-lazyload="'/img/1499231493_936.gif'" />
+    <img v-customize-lazyload="'/img/test-img/1499231493_936.gif'" />
   </div>
 </template>
