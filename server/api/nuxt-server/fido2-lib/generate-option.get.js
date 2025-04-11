66008ee98961e08127c3e614c99af5180a690d69
Parker <archer102125220.2015@gmail.com>
2025-04-12 00:46:08 +0800

    [fix]: fido2-lib bug fix

--- server/api/nuxt-server/fido2-lib/generate-option.get.js
+++ server/api/nuxt-server/fido2-lib/generate-option.get.js
@@ -30,11 +30,6 @@ export default defineEventHandler(async (event) => {
     output = {
       ...publicKeyCredentialCreationOptions,
       challenge: base64Js.fromUint8Array(publicKeyCredentialCreationOptions.challenge, true),
-      user: {
-        id: query.userId,
-        name: query.userName,
-        displayName: query.userDisplayName
-      }
     };
   } else {
     // let publicKeyCredentialCreationOptions = {};
@@ -61,11 +56,11 @@ export default defineEventHandler(async (event) => {
     output = {
       ...publicKeyCredentialCreationOptions,
       challenge: base64Js.fromUint8Array(publicKeyCredentialCreationOptions.challenge, true),
-      // user: {
-      //   id: query.userId,
-      //   name: query.userName,
-      //   displayName: query.userDisplayName
-      // }
+      user: {
+        id: query.userId,
+        name: query.userName,
+        displayName: query.userDisplayName
+      }
     };
   }
 
