ec768fe2c87f5d809e4b28018d47ba0f3260a74c
Parker <archer102125220.2015@gmail.com>
2025-08-06 09:50:03 +0800

    [update]: components/Selector update

--- components/Selector.vue
+++ components/Selector.vue
@@ -76,6 +76,30 @@ const props = defineProps({
   displayKey: {
     type: [String, Number],
     default: null
+  },
+  hasTransition: {
+    type: Boolean,
+    default: true
+  },
+  hasShadow: {
+    type: Boolean,
+    default: false
+  },
+  optionListTop: {
+    type: [String, Number],
+    default: null
+  },
+  optionListLeft: {
+    type: [String, Number],
+    default: null
+  },
+  optionListRight: {
+    type: [String, Number],
+    default: null
+  },
+  optionListWidth: {
+    type: [String, Number],
+    default: null
   }
 });
 const emit = defineEmits(['change', 'update:modelValue']);
@@ -87,8 +111,21 @@ const optionListHeight = ref(null);
 
 const cssVariable = computed(() => {
   const _cssVariable = {};
+
   if (typeof optionListHeight.value === 'number') {
-    _cssVariable['--select_option_list_top'] = '100%';
+    if (typeof props.optionListTop === 'number') {
+      _cssVariable['--select_option_list_top'] =
+        `calc(100% + ${props.optionListTop}px)`;
+    } else if (
+      typeof props.optionListTop === 'string' &&
+      props.optionListTop !== ''
+    ) {
+      _cssVariable['--select_option_list_top'] =
+        `calc(100% + ${props.optionListTop})`;
+    } else {
+      _cssVariable['--select_option_list_top'] = '100%';
+    }
+
     if (isOptionListOpen.value === true) {
       _cssVariable['--select_option_list_heigth'] =
         `${optionListHeight.value}px`;
@@ -102,8 +139,42 @@ const cssVariable = computed(() => {
 
   if (isOptionListOpen.value === true) {
     _cssVariable['--selector_arrow_icon'] = 'rotate(180deg)';
+
+    if (props.hasShadow === false) {
+      _cssVariable['--select_option_list_border_radius'] = '4px';
+      _cssVariable['--select_option_list_border'] = '1px solid #d5d5d5';
+    }
   } else {
     _cssVariable['--selector_arrow_icon'] = 'rotate(0deg)';
+  }
+
+  if (props.hasShadow === true) {
+    _cssVariable['--select_option_list_shadow'] = '-1px 3px 6px 0px #00000033';
+  }
+  if (props.hasTransition === true) {
+    _cssVariable['--select_suffix_arrow_icon_transition'] =
+      'transform 0.3s ease-in-out';
+    _cssVariable['--select_option_list_transition'] = 'height 0.3s ease-in-out';
+    _cssVariable['--select_option_list_item_selsected_transition'] =
+      'color, background-color 0.4s ease-in-out';
+  }
+
+  if (typeof props.optionListLeft === 'number') {
+    _cssVariable['--select_option_list_left'] = `${props.optionListLeft}px`;
+  } else if (typeof props.optionListLeft === 'string') {
+    _cssVariable['--select_option_list_left'] = props.optionListLeft;
+  }
+
+  if (typeof props.optionListRight === 'number') {
+    _cssVariable['--select_option_list_right'] = `${props.optionListRight}px`;
+  } else if (typeof props.optionListRight === 'string') {
+    _cssVariable['--select_option_list_right'] = props.optionListRight;
+  }
+
+  if (typeof props.optionListWidth === 'number') {
+    _cssVariable['--select_option_list_width'] = `${props.optionListWidth}px`;
+  } else if (typeof props.optionListWidth === 'string') {
+    _cssVariable['--select_option_list_width'] = props.optionListWidth;
   }
 
   return _cssVariable;
@@ -259,14 +330,24 @@ function handleChange(option, index) {
   &-option_list {
     position: absolute;
     top: var(--select_option_list_top, 500vh);
+    left: var(--select_option_list_left);
+    right: var(--select_option_list_right);
     z-index: 2;
-    width: 100%;
+    width: var(--select_option_list_width, 100%);
     height: var(--select_option_list_heigth);
     max-height: 45vh;
     overflow: auto;
     background-color: #fff;
-    box-shadow: -1px 3px 6px 0px rgba(0, 0, 0, 0.2);
-    transition: height 0.3s ease-in-out;
+
+    // box-shadow: -1px 3px 6px 0px #00000033;
+    box-shadow: var(--select_option_list_shadow);
+
+    border-radius: var(--select_option_list_border_radius);
+    border: var(--select_option_list_border);
+
+    // transition: height 0.3s ease-in-out;
+    transition: var(--select_option_list_transition);
+
     &-item {
       padding: 8px;
 
@@ -282,9 +363,8 @@ function handleChange(option, index) {
     &-item_selsected {
       color: $primary;
       background-color: $secondary;
-      transition:
-        color,
-        background-color 0.4s ease-in-out;
+      // transition: color, background-color 0.4s ease-in-out;
+      transition: var(--select_option_list_item_selsected_transition);
     }
     &-item_empty {
       padding: 8px;
@@ -305,7 +385,8 @@ function handleChange(option, index) {
     flex-direction: column;
     justify-content: center;
     &-arrow_icon {
-      transition: transform 0.3s ease-in-out;
+      // transition: transform 0.3s ease-in-out;
+      transition: var(--select_suffix_arrow_icon_transition);
       transform: var(--selector_arrow_icon, rotate(0deg));
     }
   }
