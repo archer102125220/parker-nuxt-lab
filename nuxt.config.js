26dab23aa9e97313635d9f3a70c2478ea466c60c
Parker <archer102125220.2015@gmail.com>
2025-08-26 10:11:31 +0800

    update: Firebase admin test

--- nuxt.config.js
+++ nuxt.config.js
@@ -283,7 +283,7 @@ export default defineNuxtConfig({
       // crossOriginOpenerPolicy: 'same-origin',
       // crossOriginResourcePolicy: 'cross-origin',
       permissionsPolicy: {
-        notifications: ['self'], // 允許同源使用通知
+        // notifications: ['self'], // 允許同源使用通知
         accelerometer: ['self', '"https://*.youtube.com"'], // 允許同源和YouTube使用加速計
         autoplay: ['self', '"https://*.youtube.com"'],      // 允許同源和YouTube自動播放媒體
         camera: ['self', '"https://*.youtube.com"'],       // 允許同源使用攝影機和YouTube自動播放媒體
