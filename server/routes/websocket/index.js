663cdc6cfa1d83526046d3593b97f65f8d4788af
Parker <archer102125220.2015@gmail.com>
2025-09-27 21:52:06 +0800

    update: websocket test

--- server/routes/websocket/index.js
+++ server/routes/websocket/index.js
@@ -1,40 +1,38 @@
 // https://nitro.build/guide/websocket
 // https://hub.nuxt.com/docs/features/realtime
 
-export default defineEventHandler({
-  websocket: {
-    open(peer) {
-      console.log('[ws] Default WebSocket open');
+export default defineWebSocketHandler({
+  open(peer) {
+    console.log('[ws] Default WebSocket open');
 
-      // We subscribe to the 'default' channel
-      peer.subscribe('default');
-      // We publish the number of connected users to the 'default' channel
-      peer.publish('default', peer.peers.size);
-      // We send the number of connected users to the client
-      peer.send(peer.peers.size);
-    },
-
-    message(peer, message) {
-      // console.log("[ws] Default WebSocket message", peer, message);
-      console.log("[ws] Default WebSocket message", message);
-      if (message.text().includes("ping")) {
-        peer.send("pong");
-      }
-    },
-
-    close(peer) {
-      console.log('[ws] Default WebSocket close');
-
-      peer.unsubscribe('default');
-      // Wait 500ms before sending the updated locations to the server
-      setTimeout(() => {
-        peer.publish('default', peer.peers.size);
-      }, 500);
-    },
-
-    error(peer, error) {
-      // console.log("[ws] Default WebSocket error", peer, error);
-      console.log("[ws] Default WebSocket error", error);
-    },
+    // We subscribe to the 'default' channel
+    peer.subscribe('default');
+    // We publish the number of connected users to the 'default' channel
+    peer.publish('default', peer.peers.size);
+    // We send the number of connected users to the client
+    peer.send(peer.peers.size);
   },
-})
+
+  message(peer, message) {
+    // console.log("[ws] Default WebSocket message", peer, message);
+    console.log("[ws] Default WebSocket message", message);
+    if (message.text().includes("ping")) {
+      peer.send("pong");
+    }
+  },
+
+  close(peer) {
+    console.log('[ws] Default WebSocket close');
+
+    peer.unsubscribe('default');
+    // Wait 500ms before sending the updated locations to the server
+    setTimeout(() => {
+      peer.publish('default', peer.peers.size);
+    }, 500);
+  },
+
+  error(peer, error) {
+    // console.log("[ws] Default WebSocket error", peer, error);
+    console.log("[ws] Default WebSocket error", error);
+  },
+});
--- server/routes/websocket/web-rtc.js
+++ server/routes/websocket/web-rtc.js
@@ -1,38 +1,36 @@
-export default defineEventHandler({
-  websocket: {
-    open(peer) {
-      console.log('[ws] webRTC WebSocket open');
+export default defineWebSocketHandler({
+  open(peer) {
+    console.log('[ws] webRTC WebSocket open');
 
-      // We subscribe to the 'webrtc' channel
-      peer.subscribe('webrtc');
-      // We publish the number of connected users to the 'webrtc' channel
-      peer.publish('webrtc', peer.peers.size);
-      // We send the number of connected users to the client
-      peer.send(peer.peers.size);
-    },
-
-    message(peer, message) {
-      // console.log("[ws] webRTC WebSocket message", peer, message);
-      console.log("[ws] webRTC WebSocket message", message);
-      if (message.text().includes("ping")) {
-        peer.send("pong");
-      }
-    },
-
-    close(peer) {
-      console.log('[ws] webRTC WebSocket close');
-
-      peer.unsubscribe('webrtc');
-      // Wait 500ms before sending the updated locations to the server
-      setTimeout(() => {
-        peer.publish('webrtc', peer.peers.size);
-      }, 500);
-    },
-
-
-    error(peer, error) {
-      // console.log("[ws] Default WebSocket error", peer, error);
-      console.log("[ws] webRTC WebSocket error", error);
-    },
+    // We subscribe to the 'webrtc' channel
+    peer.subscribe('webrtc');
+    // We publish the number of connected users to the 'webrtc' channel
+    peer.publish('webrtc', peer.peers.size);
+    // We send the number of connected users to the client
+    peer.send(peer.peers.size);
   },
-})
+
+  message(peer, message) {
+    // console.log("[ws] webRTC WebSocket message", peer, message);
+    console.log("[ws] webRTC WebSocket message", message);
+    if (message.text().includes("ping")) {
+      peer.send("pong");
+    }
+  },
+
+  close(peer) {
+    console.log('[ws] webRTC WebSocket close');
+
+    peer.unsubscribe('webrtc');
+    // Wait 500ms before sending the updated locations to the server
+    setTimeout(() => {
+      peer.publish('webrtc', peer.peers.size);
+    }, 500);
+  },
+
+
+  error(peer, error) {
+    // console.log("[ws] Default WebSocket error", peer, error);
+    console.log("[ws] webRTC WebSocket error", error);
+  },
+});
