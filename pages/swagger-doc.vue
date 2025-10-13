7dbd42097294064071b9e769619d2835f647b874
Parker <archer102125220.2015@gmail.com>
2025-10-13 22:06:54 +0800

    fix: swagger

--- pages/swagger-doc.vue
+++ pages/swagger-doc.vue
@@ -1,37 +1,28 @@
 <template>
-  <div id="swagger-ui" />
+  <main ref="swaggerUI" />
 </template>
 
 <script setup>
+const swaggerUI = useTemplateRef('swaggerUI');
+
+// https://www.npmjs.com/package/swagger-ui-dist
 // https://github.com/nuxt/nuxt/discussions/16165
-useHead({
-  script: [
-    {
-      type: 'text/javascript',
-      src: 'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.11.1/swagger-ui-bundle.min.js',
-      async: true
-    },
-    {
-      type: 'text/javascript',
-      src: 'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.11.1/swagger-ui-standalone-preset.min.js',
-      async: true
-    }
-  ],
-  link: [
-    {
-      rel: 'stylesheet',
-      href: 'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.11.1/swagger-ui.min.css'
-    }
-  ]
-});
-if (import.meta.client) {
+onMounted(async () => {
+  const { SwaggerUIBundle, SwaggerUIStandalonePreset } = await import(
+    'swagger-ui-dist'
+  );
+
   const ui = SwaggerUIBundle({
     url: `${import.meta.env.VITE_DOMAIN}/api/nuxt-server/swagger-docs`,
-    dom_id: '#swagger-ui',
+    domNode: swaggerUI.value,
     deepLinking: true,
     presets: [SwaggerUIBundle.presets.apis, SwaggerUIStandalonePreset],
     layout: 'StandaloneLayout'
   });
   window.ui = ui;
-}
+});
 </script>
+
+<style lang="scss">
+@import 'swagger-ui-dist/swagger-ui.css';
+</style>
