f724281612a87b1d3c9876f8739cc4da875913cf
Parker <archer102125220.2015@gmail.com>
2025-11-13 15:30:05 +0800

    update: skeleton-loader test

---
+++ app/components/SkeletonLoader.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="skeleton_loader" />
+</template>
+
+<style lang="scss">
+.skeleton_loader {
+  aspect-ratio: 1;
+  border-radius: 1rem;
+
+  background: repeating-linear-gradient(
+    135deg,
+    #ededed 30%,
+    #dcdcdc 50%,
+    #ededed 70%
+  );
+  background-size: 400%;
+  animation: skeleton_animation 2.5s infinite linear;
+}
+
+@keyframes skeleton_animation {
+  0% {
+    background-position: 100% 100%;
+  }
+  50% {
+    background-position: 0 0;
+  }
+  100% {
+    background-position: -100% -100%;
+  }
+}
+</style>
---
+++ app/pages/components/skeleton-loader.vue
@@ -0,0 +1,3 @@
+<template>
+  <SkeletonLoader />
+</template>
