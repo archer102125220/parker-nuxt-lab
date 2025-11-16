fdd51b4d5cdd2c43efb702e4a045cf1189036919
Parker <archer102125220.2015@gmail.com>
2025-11-16 23:30:54 +0800

    update: page scroll-fetch-test -> scroll-fetch (redirect)

--- app/middleware/01.redirect.global.js
+++ app/middleware/01.redirect.global.js
@@ -2,5 +2,7 @@ export default defineNuxtRouteMiddleware(async (to, from) => {
   const safeUrl = to?.href || '';
   if (['/home', '/zh/home', '/zh-tw/home', '/en/home'].includes(safeUrl)) {
     return await navigateTo('/');
+  } else if (['/components/scroll-fetch-test', '/zh/components/scroll-fetch-test', '/zh-tw/components/scroll-fetch-test', '/en/components/scroll-fetch-test']) {
+    return await navigateTo('/components/scroll-fetch');
   }
 });
