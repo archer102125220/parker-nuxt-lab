c2483aa8db3c7d334b8560badce8c2b9a1ede907
Parker <archer102125220.2015@gmail.com>
2025-12-04 00:34:09 +0800

    test: CSP test

--- app/composables/useSecurityNonce.js
+++ app/composables/useSecurityNonce.js
@@ -6,8 +6,14 @@ export function useSecurityNonce(nonceStateKey = NONCE_STATE_KEY) {
   if (import.meta.server) {
     const event = useRequestEvent();
 
+    console.log(event?.context?.security?.nonce);
+    console.log(event?.node?.res?.locals?.nonce);
+    console.log(event?.context?.security?.nonce || event?.node?.res?.locals?.nonce);
+
     nonceValue.value = event?.context?.security?.nonce || event?.node?.res?.locals?.nonce || null;
   }
+
+  console.log(nonceValue.value);
 
   return nonceValue;
 }
