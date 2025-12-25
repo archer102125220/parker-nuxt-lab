a00934fded1d31cefebad6295a4b54807213ca31
Parker <archer102125220.2015@gmail.com>
2025-12-25 11:03:02 +0800

    feat: add global middleware for dynamic layout switching based on route name or path

--- app/middleware/03.layout-switch.global.js
+++ app/middleware/03.layout-switch.global.js
@@ -1,32 +1,33 @@
 const LAYOUT_SETTING = [
-  { name: 'index', layout: 'home' },
-  { name: 'home', layout: 'home' },
-  { name: 'swagger-doc', layout: false },
-  { path: '/css-drawing/triangle-full-test', layout: 'full-screen' },
-  { path: '/css-drawing/triangle-full-test', layout: 'full-screen' }
-];
-
-export default defineNuxtRouteMiddleware((to) => {
-  const getRouteBaseName = useRouteBaseName();
-  const getLocalePath = useLocalePath();
-
-  const newLayoutName = LAYOUT_SETTING.find(({ path, exact, name }) => {
-    const toHrefLocalePath =
-      typeof path === 'string' && path !== '' ? getLocalePath(path) : undefined;
-    // const toHrefLocalePath = typeof path === 'string' && path !== '' ? path : undefined;
-    // console.log({ toHrefLocalePath });
-
-    return (
-      (exact === true
-        ? toHrefLocalePath === to.href
-        : to.href?.includes(toHrefLocalePath)) || getRouteBaseName(to) === name
+    { name: 'index', layout: 'home' },
+    { name: 'home', layout: 'home' },
+    // { name: 'swagger-doc', layout: false },
+    { path: '/css-drawing/triangle-full-test', layout: 'full-screen' },
+    { path: '/css-drawing/triangle-full-test', layout: 'full-screen' }
+  ];
+  
+  export default defineNuxtRouteMiddleware((to) => {
+    const getRouteBaseName = useRouteBaseName();
+    const getLocalePath = useLocalePath();
+  
+    const newLayoutName = LAYOUT_SETTING.find(({ path, exact, name }) => {
+      const toHrefLocalePath =
+        typeof path === 'string' && path !== '' ? getLocalePath(path) : undefined;
+      // const toHrefLocalePath = typeof path === 'string' && path !== '' ? path : undefined;
+      // console.log({ toHrefLocalePath });
+  
+      return (
+        (exact === true
+          ? toHrefLocalePath === to.href
+          : to.href?.includes(toHrefLocalePath)) || getRouteBaseName(to) === name
+      );
+      // to.name === name
+    })?.layout;
+  
+    setPageLayout(
+      typeof newLayoutName === 'string' || newLayoutName === false
+        ? newLayoutName
+        : 'default'
     );
-    // to.name === name
-  })?.layout;
-
-  setPageLayout(
-    typeof newLayoutName === 'string' || newLayoutName === false
-      ? newLayoutName
-      : 'default'
-  );
-});
+  });
+  
