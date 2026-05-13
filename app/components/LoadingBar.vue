a2bbf0fb088f85081635e87400854ecda089e75b
Parker Chen <archer102125220.2015@gmail.com>
2026-05-13 14:59:07 +0800

    feat: add configurable position props for top, bottom, left, and right to LoadingBar component

--- app/components/LoadingBar.vue
+++ app/components/LoadingBar.vue
@@ -14,6 +14,10 @@ const props = defineProps({
   loading: { type: Boolean, default: false },
   position: { type: String, default: 'absolute' },
   color: { type: String, default: 'var(--primary)' },
+  top: { type: [String, Number], default: '0' },
+  bottom: { type: [String, Number], default: null },
+  left: { type: [String, Number], default: '0' },
+  right: { type: [String, Number], default: null },
   zIndex: { type: [String, Number], default: '10' },
   height: { type: [String, Number], default: '6' },
   width: { type: [String, Number], default: '100%' }
@@ -40,6 +44,42 @@ const cssVariable = computed(() => {
   }
   newCssVariable['--loading_width'] = width;
 
+  let top = null;
+  if (typeof props.top === 'number' || /^\d+$/.test(props.top)) {
+    top = `${props.top}px`;
+  } else if (typeof props.top === 'string') {
+    top = props.top;
+  }
+  if (top !== null) {
+    newCssVariable['--loading_top'] = top;
+  }
+
+  let bottom = null;
+  if (typeof props.bottom === 'number' || /^\d+$/.test(props.bottom)) {
+    bottom = `${props.bottom}px`;
+  } else if (typeof props.bottom === 'string') {
+    bottom = props.bottom;
+  }
+  newCssVariable['--loading_bottom'] = bottom;
+
+  let left = null;
+  if (typeof props.left === 'number' || /^\d+$/.test(props.left)) {
+    left = `${props.left}px`;
+  } else if (typeof props.left === 'string') {
+    left = props.left;
+  }
+  if (left !== null) {
+    newCssVariable['--loading_left'] = left;
+  }
+
+  let right = null;
+  if (typeof props.right === 'number' || /^\d+$/.test(props.right)) {
+    right = `${props.right}px`;
+  } else if (typeof props.right === 'string') {
+    right = props.right;
+  }
+  newCssVariable['--loading_right'] = right;
+
   let zIndex = 10;
   if (
     typeof props.zIndex === 'number' ||
@@ -57,8 +97,10 @@ const cssVariable = computed(() => {
 .loading_bar {
   // Positioning
   position: var(--loading_position);
-  top: 0;
-  left: 0;
+  top: var(--loading_top);
+  bottom: var(--loading_bottom);
+  left: var(--loading_left);
+  right: var(--loading_right);
   z-index: var(--loading_zIndex);
 
   // Display & Box Model
