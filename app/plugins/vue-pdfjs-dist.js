86f5e2a15c272389850efe2cef316a2c7261c5d0
Parker Chen <archer102125220.2015@gmail.com>
2026-05-21 10:06:41 +0800

    feat: add PdfReader component with pdf.js integration and necessary dependencies

---
+++ app/plugins/vue-pdfjs-dist.js
@@ -0,0 +1,14 @@
+// TODO
+// import pdfReader from '@app/utils/third-party/pdf-reader';
+
+// const { PDFViewerApplication, AppOptions } = pdfReader;
+
+export default defineNuxtPlugin(() => {
+
+  return {
+    provide: {
+      // pdfViewer:PDFViewerApplication,
+      // pdfViewerAppOptions: AppOptions
+    }
+  };
+});
